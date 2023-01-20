b13d942f00ebb81a8f2ce2ce162b91c1d500a666
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-20 10:47:26 +0100

    Add project files.

---
+++ prog/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int num = int.Parse(Console.ReadLine());
+
+            int[] v = new int[num];
+
+            Console.WriteLine($"Skriv in {num} heltal:");
+
+            for (int i = 0; i < num; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            int tal = 0;
+            Console.WriteLine("Här är de talen du skriv in:");
+            for (int i = 0; i < num; i++)
+            {
+                tal = tal + 1;
+                Console.Write($"Tal {tal}: {v[i]}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
